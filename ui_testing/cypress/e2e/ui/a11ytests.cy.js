23ffefc26b505b5f6af13b032463d964c52b3f18
Eduardo Lopez <eduardooscar.lopez@globant.com>
2023-08-25 10:04:09 -0300

    add tests and update reporter options

--- ui_testing/cypress/e2e/ui/a11ytests.cy.js
+++ ui_testing/cypress/e2e/ui/a11ytests.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 describe('Check A11y rules in different sites', () => {
-    it('Check in Google page', ()=>{
+    it.only('Check in Google page', ()=>{
         cy.visit('https://www.google.com/')
         cy.injectAxe()
         cy.checkA11y()
@@ -21,12 +21,30 @@ describe('Check A11y rules in different sites', () => {
         cy.injectAxe()
         cy.checkA11y()
     })
-    it.only('Check in Imagine Learning page', ()=>{
+    it('Check in Celeri home page', ()=>{
+        cy.visit('https://www.celeri.app/')
+        cy.injectAxe()
+        cy.checkA11y()
+    })
+    it.only('Check in Celeri agendar demo page', ()=>{
+        cy.visit('https://www.celeri.app/agendar-demo')
+        cy.injectAxe()
+        cy.checkA11y()
+    })
+    it.only('Check in articulo random', ()=>{
+        cy.visit('https://www.celeri.app/blog/desafios-de-ciso-ante-los-ciberdelitos')
+        cy.injectAxe()
+        cy.checkA11y()
+    })
+     
+
+
+    it.skip('Check in Imagine Learning page', ()=>{
         cy.visit('https://www.imaginelearning.com/')
         cy.injectAxe()
         cy.checkA11y()
     })
-    it.only('Check in Imagine Learning contact us page', ()=>{
+    it.skip('Check in Imagine Learning contact us page', ()=>{
         cy.visit('https://www.imaginelearning.com/about/contact-us/')
         cy.injectAxe()
         cy.checkA11y()
